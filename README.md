ecf6999a7cfbd9113377c0fc9701c3a32973ff13
LejayChen@github.com <fiorano1chen@gmail.com>
2017-09-25 00:29:28 -0300

    test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ASTR5400_term_project
 polytropic model fitting
+
+The toy model: using Euler method
--- project_main.py
+++ project_main.py
@@ -1,1 +1,37 @@
+#ASTR5400 term project
+#author: Lingjian Chen
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 n=3
+
+def f(z):
+    return 1 - (1/6.)*z**2 + (n/1200.)*z**4
+
+def df(z):
+    return  - (1 / 3.) * z  + (n*4 / 1200.) * z ** 3
+
+#taylor expansion region
+z = np.linspace(0,1,1000)
+w = f(z)
+dw_dz = df(z)
+
+z_now = z[-1]
+w_now = w[-1]
+dw_dz_now = dw_dz[-1]
+
+while w_now>0:
+    z_prev = z_now
+    w_prev = w_now
+    dw_dz_prev = dw_dz_now
+
+    z_now = z_prev + 0.0001
+    w_now = w_prev + dw_dz_prev*0.0001
+    #dw_dz_now = (w_now - w_prev)/(z_now - z_prev)
+
+    z = np.append(z, z_now)
+    w = np.append(w, w_now)
+
+plt.plot(z,w)
+plt.show()
